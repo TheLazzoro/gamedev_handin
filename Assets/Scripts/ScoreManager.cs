0a24172d2c482086cb7a9c4f8f457297dfb6d198
Nikolaj <nikolajweinell@hotmail.com>
2022-10-15 18:35:36 +0200

    Add score tracker for team1 and team2

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public Text scoreText;
+
+    int scoreTeam1 = 0;
+    int scoreTeam2 = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = scoreTeam1.ToString() + " - " + scoreTeam2.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
