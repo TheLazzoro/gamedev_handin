cc8309b2b0595ca622e967f3f3a66f908a540985
Tsukani <34612305+Tsukani@users.noreply.github.com>
2022-12-28 23:29:23 +0100

    Please work this time!

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -65,6 +65,7 @@ public class Goal : MonoBehaviour
         carYeeter.Yeet();
 
         // Find the actual ball and hide that (keep particles)
+        Debug.Log(GameObject.Find("Soccer Ball"));
         GameObject.Find("Soccer Ball").SetActive(false);
     }
 }
