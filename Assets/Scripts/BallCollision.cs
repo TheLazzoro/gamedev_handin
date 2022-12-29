f0860e6c6d74a576b6bc8aa3536b532fe4ddfea0
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2022-12-29 16:42:06 +0100

    Custom ball collision + downscaled ball

--- Assets/Scripts/BallCollision.cs
+++ Assets/Scripts/BallCollision.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallTriggerSFX : MonoBehaviour
+public class BallCollision : MonoBehaviour
 {
 
     public AudioSource audioSource;
     public AudioClip hit;
     public AudioClip explosion;
+
+    private Rigidbody rigidbody;
     
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -21,6 +23,14 @@ public class BallTriggerSFX : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.tag == "Player")
+        {
+            rigidbody.velocity = Vector3.zero; // reset ball position
+            Vector3 carVelocity = other.GetComponent<Rigidbody>().velocity;
+            Vector3 force = carVelocity * 100;
+            rigidbody.AddForce(force); // apply custom force
+        }
+
         if (other.name != "GoalTriggerArea")
         {
             audioSource.PlayOneShot(hit);
